3a15cf89c2f79163718ef9759c9a47083b4820be
Jesus Rosas <jesusdrosas96@gmail.com>
2020-01-09 01:37:50 -0600

    clase 10 - condicionales

---
+++ clase10.js
@@ -0,0 +1,44 @@
+var jesus = {
+  nombre: 'Jesus',
+  apellido: 'Rosas',
+  edad: 23,
+  ingeniero: false,
+  cocinero: false,
+  cantante: true,
+  dj: false,
+  guitarrista: false,
+  drone: false
+}
+
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`)
+
+  if (persona.ingeniero) {
+    console.log('Ingeniero')
+  } else {
+    console.log('No es ingeniero')
+  }
+
+  if (persona.cocinero) {
+    console.log('Cocinero')
+  }
+
+  if (persona.dj) {
+    console.log('DJ')
+  }
+
+  if (persona.cantante) {
+    console.log('Cantante')
+  }
+
+  if (persona.guitarrista) {
+    console.log('Gutiarrista')
+  }
+
+  if (persona.drone) {
+    console.log('Piloto de drone')
+  }
+
+}
+
+imprimirProfesiones(jesus)
