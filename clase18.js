2c3154e5634632cc2e3fff3e85682266e5a09774
Jesus Rosas <jesusdrosas96@gmail.com>
2020-01-09 02:25:51 -0600

    clase 17 - 20

---
+++ clase18.js
@@ -0,0 +1,46 @@
+var jesus = {
+  nombre: 'Jesus',
+  apellido: 'Rosas',
+  altura: 1.73
+}
+
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Perez',
+  altura: 1.86
+}
+
+var martin = {
+  nombre: 'Martin',
+  apellido: 'Gomez',
+  altura: 1.85
+}
+
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.71
+}
+
+var vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56
+}
+
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76
+}
+
+const esAlta = ({ altura }) => altura > 1.8
+
+var personas = [jesus, alan, martin, dario, vicky, paula]
+
+var personasAltas = personas.filter(esAlta)
+// var personasAltas = personas.filter(function (persona) {
+//   return persona.altura > 1.8
+// })
+
+console.log(personasAltas)
